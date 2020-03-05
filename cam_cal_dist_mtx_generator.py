0270785ae11724288f3047b3ec5e220e2ef64265
alizadehali <a.alizade@live.com>
2020-03-05 16:19:17 +0300

    initial commit

---
+++ cam_cal_dist_mtx_generator.py
@@ -0,0 +1,46 @@
+import numpy as np
+from tqdm import tqdm
+import cv2
+import glob
+import os
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+for dir in tqdm(os.listdir('multi_cam')):
+    images = glob.glob(os.path.join(dir, '*.png'))
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((9*6,3), np.float32)
+    objp[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    for fname in tqdm(images):
+        img = cv2.imread(fname)
+        # img = img[400:1000, 600:1200, :]
+        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv2.findChessboardCorners(gray, (9,6),None)
+
+        # If found, add object points, image points (after refining them)
+        print(ret)
+        if ret == True:
+            objpoints.append(objp)
+
+            corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+            imgpoints.append(corners2)
+
+            # Draw and display the corners
+            img = cv2.drawChessboardCorners(img, (9,6), corners2,ret)
+            cv2.imshow('img',img)
+            cv2.waitKey(500)
+    cv2.destroyAllWindows()
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+    np.save(os.path.join('multi_cam', os.path.join(dir, 'mtx.npy')), mtx)
+    np.save(os.path.join('multi_cam', os.path.join(dir, 'dist.npy')), dist)
+
+
+
+
---
+++ cam_intrinsic_cal.py
@@ -0,0 +1,42 @@
+# load modules
+import argparse
+import glob
+import os
+from tqdm import tqdm
+import numpy as np
+import cv2
+
+
+__author__ = "Ali Alizadeh"
+__email__ = 'aalizade@ford.com.tr / a.alizade@live.com'
+__license__ = 'AA_intrinsic_cal'
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Multi-camera intrinsic calibration')
+    parser.add_argument('-i', '--input_dir', default='multi_cam', help='Parent directory of the original images')
+    parser.add_argument('-o', '--output_dir', default='multi_cam', help='Directory of the undistorted images')
+    args = parser.parse_args()
+    return args
+
+def main(in_dir, out_dir):
+    for dir in tqdm(os.listdir(in_dir)):
+        mtx = np.load(os.path.join(in_dir+ '/' + dir, 'mtx.npy'))
+        dist = np.load(os.path.join(in_dir+ '/' + dir, 'dist.npy'))
+        images = glob.glob(in_dir + '/' + os.path.join(dir, '*.png'))
+        for fname in tqdm(images):
+            print('fname: ', fname)
+            print('imwrite: ', os.path.join(out_dir, fname.split('/')[-1]))
+            img = cv2.imread(fname)
+            # undistort
+            dst = cv2.undistort(img, mtx, dist, None, mtx)
+            # save image
+            cv2.imwrite(os.path.join(out_dir + '/' + dir, fname.split('/')[-1]), dst)
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    in_dir = args.input_dir
+    out_dir = args.output_dir
+    main(in_dir, out_dir)
+
+
+
